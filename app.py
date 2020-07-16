5b8a602d9e08528ee43bee48498aefb9cc3552f6
kavibharathi2000 <67953348+kavibharathi2000@users.noreply.github.com>
2020-07-16 17:19:06 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,47 @@
+import numpy as np
+import os
+from keras.models import load_model
+from keras.preprocessing import image
+import tensorflow as tf
+import tensorflow.keras.get_default_graph
+global graph
+graph = tf.get_default_graph()
+from flask import Flask , request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+
+app = Flask(__name__)
+model = load_model("project.h5")
+
+@app.route('/')
+def index():
+    return render_template('base.html')
+@app.route('/predict',methods = ['GET','POST'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['image']
+        print("current path")
+        basepath = os.path.dirname(__file__)
+        print("current path", basepath)
+        filepath = os.path.join(basepath,'uploads',f.filename)
+        print("upload folder is ", filepath)
+        f.save(filepath)
+        
+        img = image.load_img(filepath,target_size = (300,300))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x,axis =0)
+        
+        with graph.as_default():
+            pred = model.predict_classes(x)
+            
+            
+            print("prediction",pred)
+            
+        index = ['cardboards','glass','metals','papers','plastic','Trash']
+        
+        text = "the predicted classification  is : " + str(index[pred[0]])
+        
+    return text
+if __name__ == '__main__':
+    app.run(debug = True, threaded = False)
+        
